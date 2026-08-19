89cda1aa8edc76984a1f8ce9d112f52c5bab5653
Yajuddin Raju <yajuddinraju1@gmail.com>
2026-08-19 03:41:56 -0400

    add:better auth login & sigup authentication

---
+++ src/lib/auth.ts
@@ -0,0 +1,18 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+
+export const auth = betterAuth({
+  // 
+  trustedOrigins: [
+    "http://localhost:3000",
+  ],
+
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+
+  emailAndPassword: {
+    enabled: true,
+  },
+});
