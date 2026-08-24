5e7cf9a402b96193ebe5bdaf86eba6d9115e51c6
Lam272 <ayatlam787@gmail.com>
2026-08-24 22:35:57 +0600

    update dashboard and banner image

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,6 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 
 export const auth = betterAuth({
-  // 
   trustedOrigins: [
     "http://localhost:3000",
   ],
@@ -14,5 +13,17 @@ export const auth = betterAuth({
 
   emailAndPassword: {
     enabled: true,
+  },
+
+  // Role field-ti sothik vabe save ar fetch korar jonno
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        required: false,
+        defaultValue: "user", // choto okkhor-e default "user"
+        input: true,        // register form theke input receive korbe
+      },
+    },
   },
 });
